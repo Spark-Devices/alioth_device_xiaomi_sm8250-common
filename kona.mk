0fe2656e7278cd8f359512c9e840080df5001d34
∂α૨ҡ_ρɦσεɳเא <dark.phnx12@gmail.com>
2023-03-17 16:10:46 +0530

    Revert "sm8250-common: Import and switch to QTI Perf system"
    This reverts commit 99decfed477d3b986d4adade719b6cf82f10207c.

--- kona.mk
+++ kona.mk
@@ -453,19 +453,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0 \
-    vendor.qti.hardware.perf@2.2 \
-    vendor.qti.hardware.perf@2.2.vendor \
-    vendor.qti.hardware.perf@2.3 \
-    libpsi.vendor \
-    libtflite \
-    vendor.qti.hardware.servicetracker@1.2.vendor
-
-# Perf Boot Jars
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    QXPerformance \
-    UxPerformance
+    libqti-perfd-client
 
 # Power
 PRODUCT_PACKAGES += \
